79bc9e000dc342750e80b547d9eb24e4d96c31d8
Beth Hart <errordrivendev@gmail.com>
2018-02-06 02:24:11 -0800

    set up file tree, views, controllers, repositories

---
+++ UsingViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsingViewComponents.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+
+        private IProductRepository repository;
+
+        //ctor with dependency on IProductRepository. When Mvc inits controller, it will consult service provider in setup, which will pass in the IPR object.
+        public HomeController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+
+        //following 2 methods use the Post/redirect/Get pattern for handling form data
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            repository.AddProduct(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+    
+}
+
--- UsingViewComponents/Startup.cs
+++ UsingViewComponents/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using UsingViewComponents.Models;
 
 namespace UsingViewComponents
 {
@@ -15,6 +16,10 @@ namespace UsingViewComponents
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //specifies that one instance of IPR and ICR will be created and used whereever needed
+            services.AddSingleton<IProductRepository, MemoryProductRepository>();
+            services.AddSingleton<ICityRepository, MemoryCityRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +30,8 @@ namespace UsingViewComponents
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
